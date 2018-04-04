8b7f548e5650c800a786f3072c3bbdfd4f27087e
Glitch (ethereal-carbon) <none>
2018-04-04 20:49:36 +0000

    🌔🍩 Checkpoint ./public/style.css:587738/183 ./views/search.pug:587738/5 ./public/js/app.js:587738/136

--- public/js/app.js
+++ public/js/app.js
@@ -2,10 +2,12 @@
 
 $(document).ready(function () {
   console.log(services);
+  const searchItem = [];
   $('#searchField').keyup(function(event){
     this.value.length>2?services.getDomainSuggestion(this.value)
           .then(data=>{
             console.log(data);
+            $('#search-results').html();
             (data.data || []).forEach(val=>{
               $('#search-results').append('<li class="list-group-item" value='+val.domain+'>'+val.name+'</li>')
             });
@@ -15,6 +17,8 @@ $(document).ready(function () {
   
    $('#search-results').click(event=>{
      console.log(event);
+     $(event.target).attr(;;)
+     
    });
   
 })
