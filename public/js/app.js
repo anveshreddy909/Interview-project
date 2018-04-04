4b9bd6b1cea1d87257647fdffd96f79ddd756aff
Glitch (ethereal-carbon) <none>
2018-04-04 21:10:37 +0000

    ⭐️👐 Checkpoint ./public/js/app.js:587738/130 ./public/style.css:587738/25 ./views/search.pug:587738/212

--- public/js/app.js
+++ public/js/app.js
@@ -7,21 +7,22 @@ $(document).ready(function () {
     this.value.length>2?services.getDomainSuggestion(this.value)
           .then(data=>{
             console.log(data);
-            $('#search-results').html();
+            $('#search-results').html("");
             (data.data || []).forEach(val=>{
               $('#search-results').append('<li class="list-group-item" value='+val.domain+'>'+val.name+'</li>')
             });
-            $('#search-results').toggle();
-          }): null;
+            $('#search-results').show();
+          }): $('#search-results').hide();
   });
   
    $('#search-results').click(event=>{
      console.log($(event.target).attr('value'));
      searchItem.push({value: $(event.target).attr('value'), label: event.target.innerHTML});
      console.log(searchItem);
-     $('#search-results').toggle();
+     $('#search-results').hide();
      $('.searchVal').html(searchItem.reduce((finalVal, val)=>{return (finalVal? finalVal+ ", "+ val.label : val.label)},""));
-     $('#searchField').value="";
+     $('#searchField').val("");
+     $('#searchField').attr('placeholder', "");
    });
   
 })
