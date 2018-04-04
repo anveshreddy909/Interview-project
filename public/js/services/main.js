75b9b4dd86340f5c3d6abaddbd5739414d203502
Glitch (ethereal-carbon) <none>
2018-04-04 19:15:30 +0000

    🎾😜 Checkpoint ./public/js/services/main.js:587738/307 ./public/js/app.js:587738/175 ./views/includes/foot.jade:587738/109 ./public/js/lib/jquery.js:587738/93228

--- public/js/services/main.js
+++ public/js/services/main.js
@@ -1,8 +1,10 @@
-export default {
+const services = {
   
   getDomainSuggestion: function(input) {
     return fetch('https://hunter.io/v2/domains-suggestion?query='+input)
-           .then()
+          .then(response=>response.json())
+          .catch(e=>Promise.reject("cities call failed"))
+          .then(response=>Promise.resolve(response))
   }
   
 }
