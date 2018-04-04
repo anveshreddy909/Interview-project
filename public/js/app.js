f3f7c784b9cea30309ac9ecdb28449c30e292f6e
Glitch (ethereal-carbon) <none>
2018-04-04 20:07:24 +0000

    🛢🏀 Checkpoint ./public/js/app.js:587738/183 ./views/search.pug:587738/50

--- public/js/app.js
+++ public/js/app.js
@@ -2,15 +2,7 @@
 
 $(document).ready(function () {
   console.log(services);
-  let tokenFieldObj = {
-    autocomplete: {
-      source: ['red'],
-      delay: 100
-    },
-    showAutocompleteOnFocus: false
-  }
-  $('#searchField').tokenfield(tokenFieldObj);
   $('#searchField').keypress(event=>{
-    console.log(event.value)
+    console.log(event)
   });
 })
