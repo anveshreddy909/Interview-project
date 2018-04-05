7ee9e22777c7bd20733f59a5e6555c6d8436e8e7
Glitch (ethereal-carbon) <none>
2018-04-05 11:12:56 +0000

    🌱🖖 Checkpoint ./public/js/services/main.js:587738/368

--- public/js/services/main.js
+++ public/js/services/main.js
@@ -22,10 +22,15 @@ const services = {
   },
   
   search: function(domain) {
-    return fetch(`https://api.hunter.io/v2/email-count?domain=${domain}`) 
+    return fetch(`https://api.hunter.io/v2/email-count?domain=${domain}`,headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    }) 
           .then(response=>response.json())
           .catch(e=>Promise.reject("search call failed"))
-          .then(response=>Promise.resolve(response))
+          .then(response=>{
+              return Promise.resolve(response)
+          })
   }
   
 }
